0b05e48627281f1338b44694a2744637badcf1f8
DESKTOP-27CQTIN\grzegurz <grzegorzstudzinski@hotmail.com>
2022-04-06 01:59:07 +0200

    some initial design

---
+++ src/CustomizedList.tsx
@@ -0,0 +1,166 @@
+import { useState } from "react";
+
+import {
+  List,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  Paper,
+  Box,
+  styled,
+  ThemeProvider,
+  createTheme,
+  ListItemIcon,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+
+const useStyles = makeStyles({
+  croppedIcon: {
+    width: "35px",
+    height: "35px",
+    objectFit: "none",
+  },
+});
+
+const data = [
+  { label: "Weight", details: false },
+  { label: "Height", details: false },
+];
+
+const FireNav = styled(List)<{ component?: React.ElementType }>({
+  "& .MuiListItemButton-root": {
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  "& .MuiListItemIcon-root": {
+    minWidth: 0,
+    marginRight: 16,
+  },
+});
+
+export default function CustomizedList() {
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
+  return (
+    <Box sx={{ padding: "15px" }}>
+      <ThemeProvider
+        theme={createTheme({
+          components: {
+            MuiListItemButton: {
+              defaultProps: {
+                disableTouchRipple: true,
+              },
+            },
+          },
+          palette: {
+            mode: "dark",
+            primary: { main: "rgb(102, 157, 246)" },
+            background: { paper: "rgb(5, 30, 52)" },
+          },
+        })}
+      >
+        <Paper elevation={0} sx={{ maxWidth: 1024, margin: "0 auto" }}>
+          <FireNav component="nav" disablePadding>
+            <ListItemButton component="a" href="#customized-list">
+              <ListItemText
+                sx={{ my: 0 }}
+                primary="Pokemon Catalog 🔥"
+                primaryTypographyProps={{
+                  fontSize: 20,
+                  fontWeight: "medium",
+                  letterSpacing: 0,
+                  align: "center",
+                }}
+              />
+            </ListItemButton>
+
+            <ListItemButton component="a" href="#customized-list">
+              <TextField
+                id="outlined-basic"
+                label="Search for pokemons"
+                variant="outlined"
+                fullWidth
+              />
+            </ListItemButton>
+
+            {[1, 2, 3, 4, 5].map((item) => (
+              <Box
+                sx={{
+                  bgcolor: open ? "rgba(71, 98, 130, 0.2)" : null,
+                  pb: open ? 2 : 0,
+                }}
+              >
+                <ListItemButton
+                  alignItems="flex-start"
+                  onClick={() => setOpen(!open)}
+                  sx={{
+                    px: 3,
+                    pt: 2.5,
+                    pb: 2.5,
+                    "&:hover, &:focus": { "& svg": { opacity: open ? 1 : 0 } },
+                  }}
+                >
+                  <ListItemIcon>
+                    <img
+                      className={classes.croppedIcon}
+                      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png"
+                      alt="spirit"
+                    />
+                  </ListItemIcon>
+
+                  <ListItemText
+                    primary="Ivysaur"
+                    primaryTypographyProps={{
+                      fontSize: 15,
+                      fontWeight: "medium",
+                      lineHeight: "20px",
+                      mb: "2px",
+                    }}
+                    secondary="RandomType"
+                    secondaryTypographyProps={{
+                      noWrap: true,
+                      fontSize: 12,
+                      lineHeight: "16px",
+                      color: "rgba(255,255,255,0.5)",
+                    }}
+                    sx={{ my: 0 }}
+                  />
+                  <KeyboardArrowDown
+                    sx={{
+                      mr: -1,
+                      opacity: 0,
+                      transform: open ? "rotate(-180deg)" : "rotate(0)",
+                      transition: "0.2s",
+                    }}
+                  />
+                </ListItemButton>
+
+                {open &&
+                  data.map((item) => (
+                    <ListItemButton
+                      key={item.label}
+                      sx={{
+                        py: 0,
+                        minHeight: 32,
+                        color: "rgba(255,255,255,.8)",
+                      }}
+                    >
+                      <ListItemText
+                        primary={item.label}
+                        primaryTypographyProps={{
+                          fontSize: 14,
+                          fontWeight: "medium",
+                        }}
+                      />
+                    </ListItemButton>
+                  ))}
+              </Box>
+            ))}
+          </FireNav>
+        </Paper>
+      </ThemeProvider>
+    </Box>
+  );
+}
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Pokemon {
+    name: string;
+    type: string;
+    sprite: string;
+    weight: number;
+    height: number;
+}
